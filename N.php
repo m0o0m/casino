522bb1fa5776a52a429513e64c075207fb727881
Skv <kirill.speransky@gmail.com>
2015-05-22 11:37:28 +0300

    cleanup

--- N.php
+++
@@ -1,9 +0,0 @@
-<?
-    $m = scandir('games/igt');
-    
-    foreach($m as $d) {
-        if(strlen($d) > 3) {
-            echo $d.'<br>';
-            unlink('games/igt/'.$d.'/index.html');
-        }
-    }
